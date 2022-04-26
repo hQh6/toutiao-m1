2872ee6782535b949b18044f2e35c9f836b08aa3
hqh <1973789245@qq.com>
2022-04-26 09:11:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>黑马头条</h1>
+    
+    <!-- 路由的出口  不能删 -->
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  // 项目的根组件，给每个组件起个名字，做组件缓存会用到name
+    name:"App"
+  }
+
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  // 配置自己的路由表
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
